b7a8f29a072b3c6c158a934b233ff1c59cc7a358
Agasthi98 <sankalana98@gmail.com>
2021-05-12 01:10:43 +0530

    error handling complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errormiddleware.js'
 import connectDB from './config/db.js'
 
 
@@ -13,11 +14,17 @@ connectDB()
 
 const app  = express()
 
+
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
