18ce0b416850e0e7604b71541e06963217cd2ece
Agasthi98 <sankalana98@gmail.com>
2021-05-13 18:48:35 +0530

    payment screen finished

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -24,7 +24,7 @@ const ShippingScreen = ({ history }) => {
     }
 
     return  <FormContainer>
-        <CheckoutSteps step1 step2 step3 step4 />
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
@@ -46,9 +46,9 @@ const ShippingScreen = ({ history }) => {
                 <Form.Label>Country</Form.Label>
                 <Form.Control type='text' placeholder='Enter country' value={country} required onChange={(e) => setCountry(e.target.value)}></Form.Control>
                 </Form.Group>
-            <button type= 'submit' varient='primary'>
+            <Button type= 'submit' varient='primary'>
                 Continue
-            </button>
+            </Button>
         </Form>
     </FormContainer>
     
