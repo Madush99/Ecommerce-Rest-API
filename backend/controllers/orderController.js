c0084370cf1d5b97c418592cc76c0632b4b77e6b
Madush99 <madusankagajadeera99@gmail.com>
2021-05-13 22:36:15 +0530

    order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import { Error } from 'mongoose'
 import Order from '../models/orderModel.js'
 
 // @desc    Create new order
@@ -35,6 +36,20 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
     res.status(201).json(createdOrder)
   }
+})
+
+
+const getOrderById = asyncHandler(async (req, res) => {
+
+  const order = await (await Order.findById(req.params.id)).populated('user', 'name', 'email')
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
 })
 
 export { addOrderItems }
