b36b9e3510f61f59f032e7087513e9429f9eff31
Madush99 <madusankagajadeera99@gmail.com>
2021-05-13 17:53:51 +0530

    css

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -22,34 +22,37 @@ const ShippingScreen = ({ history }) => {
         history.push('/payment')
     }
 
-    return  <FormContainer>
+    return <FormContainer>
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
-        <Form.Group controlId='address'>
+            <Form.Group controlId='address' className='py-1'>
                 <Form.Label>Address</Form.Label>
                 <Form.Control type='text' placeholder='Enter address' value={address} required onChange={(e) => setAddress(e.target.value)}></Form.Control>
-                </Form.Group>
+            </Form.Group>
 
-        <Form.Group controlId='city'>
+            <Form.Group controlId='city' className='py-2'>
                 <Form.Label>City</Form.Label>
                 <Form.Control type='text' placeholder='Enter city' value={city} required onChange={(e) => setCity(e.target.value)}></Form.Control>
-                </Form.Group>
+            </Form.Group>
 
-        <Form.Group controlId='postalCode'>
+            <Form.Group controlId='postalCode' className='py-2'>
                 <Form.Label>Postal Code</Form.Label>
                 <Form.Control type='text' placeholder='Enter postal Code' value={postalCode} required onChange={(e) => setPostalCode(e.target.value)}></Form.Control>
-                </Form.Group>
+            </Form.Group>
 
-        <Form.Group controlId='country'>
+            <Form.Group controlId='country' className='py-2'>
                 <Form.Label>Country</Form.Label>
                 <Form.Control type='text' placeholder='Enter country' value={country} required onChange={(e) => setCountry(e.target.value)}></Form.Control>
-                </Form.Group>
-            <button type= 'submit' varient='primary'>
-                Continue
+            </Form.Group>
+            <Form.Group className='py-3'>
+                <button type='submit' varient='primary'>
+                    Continue
             </button>
+            </Form.Group>
+
         </Form>
     </FormContainer>
-    
+
 }
 
 export default ShippingScreen
