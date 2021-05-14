7925a78fee1e02511f354918481180d7c00d9eee
Agasthi98 <sankalana98@gmail.com>
2021-05-14 16:31:12 +0530

    admin user delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,14 @@ import {
     registerUser, 
     getUserProfile, 
     updateUserProfile,
-    getUsers
+    getUsers,
+    deleteUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser)
 
 export default router
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,10 @@ import {
       USER_DETAILS_REQUEST,
       USER_DETAILS_RESET,
       USER_DETAILS_SUCCESS,
+      USER_LIST_REQUEST,
+      USER_LIST_SUCCESS,
+      USER_LIST_FAIL,
+      USER_LIST_RESET,
       USER_LOGIN_FAIL,
       USER_LOGIN_REQUEST,
       USER_LOGIN_SUCCESS,
@@ -12,7 +16,10 @@ import {
       USER_REGISTER_SUCCESS,
       USER_UPDATE_PROFILE_FAIL,
       USER_UPDATE_PROFILE_REQUEST,
-      USER_UPDATE_PROFILE_SUCCESS
+      USER_UPDATE_PROFILE_SUCCESS,
+      USER_DELETE_REQUEST,
+      USER_DELETE_SUCCESS,
+      USER_DELETE_FAIL
 } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
@@ -41,7 +48,7 @@ export const userRegisterReducer = (state = {}, action) => {
             default:
                   return state;
       }
-};
+}
 
 export const userDetailsReducer = (state = { user: {} }, action) => {
       switch (action.type) {
@@ -56,9 +63,9 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
             default:
                   return state;
       }
-};
+}
 
-export const userUpdateProfileReducer = (state = { user: {} }, action) => {
+export const userUpdateProfileReducer = (state = { }, action) => {
       switch (action.type) {
             case USER_UPDATE_PROFILE_REQUEST:
                   return { loading: true };
@@ -69,4 +76,35 @@ export const userUpdateProfileReducer = (state = { user: {} }, action) => {
             default:
                   return state;
       }
-};
+}
+
+
+export const userListReducer = (state = { users: [] }, action) => {
+      switch (action.type) {
+            case USER_LIST_REQUEST:
+                  return { loading: true };
+            case USER_LIST_SUCCESS:
+                  return { loading: false, users: action.payload };
+            case USER_LIST_FAIL:
+                  return { loading: false, error: action.payload };
+            case USER_DETAILS_RESET: 
+                  return {users: [] }
+            default:  
+                  return state;
+      }
+}
+
+
+export const userDeleteReducer = (state = { }, action) => {
+      switch (action.type) {
+            case USER_DELETE_REQUEST:
+                  return { loading: true };
+            case USER_DELETE_SUCCESS:
+                  return { loading: false, success: true};
+            case USER_DELETE_FAIL:
+                  return { loading: false, error: action.payload };
+            default:  
+                  return state;
+      }
+}
+
