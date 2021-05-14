c0bd20ed583f3f401607f82b8e8e880f29bae3c7
Madush99 <madusankagajadeera99@gmail.com>
2021-05-14 20:35:06 +0530

    Product delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,12 +18,24 @@ const getProducts = asyncHandler(async (req, res) => {
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
-    if(product) {
+    if (product) {
         res.json(product)
-    }else {
+    } else {
         res.status(404)
         throw new Error('Product not found')
     }
 })
 
-export { getProducts, getProductById }
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export { getProducts, getProductById, deleteProduct }
