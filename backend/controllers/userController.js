ab2e1b9d5a81ee1ae343107c88fbde36ed318617
Agasthi98 <sankalana98@gmail.com>
2021-05-14 14:08:23 +0530

    admin auth ok

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,4 +108,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+
+// @desc    Get all users
+// @route   GET/api/user
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -24,7 +24,7 @@ const ShippingScreen = ({ history }) => {
     }
 
     return <FormContainer>
-        <CheckoutSteps step1 step2 step3 step4 />
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address' className='py-1'>
