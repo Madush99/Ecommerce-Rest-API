560ccc1b71ff9fa86371e0ccd9d46baa3b0c0053
Madush99 <madusankagajadeera99@gmail.com>
2021-05-15 00:53:04 +0530

    Order control

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,7 +19,7 @@ import {
       userUpdateReducer,
 } from './reducers/userReducers'
 
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducer'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer, orderListReducer } from './reducers/orderReducer'
 
 const reducer = combineReducers({
       productList: productListReducer,
@@ -39,6 +39,7 @@ const reducer = combineReducers({
       orderDetails: orderDetailsReducer,
       orderPay: orderPayReducer,
       orderListMy: orderListMyReducer,
+      orderList: orderListReducer,
 
 })
 
