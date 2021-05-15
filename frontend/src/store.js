032be446596323e54e1f2e1380257a38d882a5fd
Agasthi98 <sankalana98@gmail.com>
2021-05-15 13:57:51 +0530

    product frontend review

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,8 @@ import {
       productDetailsReducer,
       productDeleteReducer,
       productCreateReducer,
-      productUpdateReducer
+      productUpdateReducer,
+      productReviewCreateReducer
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -27,6 +28,7 @@ const reducer = combineReducers({
       productDelete: productDeleteReducer,
       productCreate: productCreateReducer,
       productUpdate: productUpdateReducer,
+      productReviewCreate: productReviewCreateReducer,
       cart: cartReducer,
       userLogin: userLoginReducer,
       userRegister: userRegisterReducer,
