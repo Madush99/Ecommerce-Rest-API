e0b4c85bb43a502fbae0c83de5be2b8b26253378
Agasthi98 <sankalana98@gmail.com>
2021-05-17 14:17:13 +0530

    cart reset success  🔥

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,11 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_DELIVERY_METHOD } from '../constants/cartConstants'
+import { 
+  CART_ADD_ITEM, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_PAYMENT_METHOD, 
+  CART_SAVE_SHIPPING_ADDRESS,
+   CART_SAVE_DELIVERY_METHOD,
+   CART_CLEAR_ITEMS,
+  } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, 
   action
@@ -43,6 +50,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} },
             ...state,
             deliveryMethod: action.payload,
           }  
+     case CART_CLEAR_ITEMS:
+            return {
+              ...state,
+              cartItems: [],
+            }
     default:
       return state
   }
