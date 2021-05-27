d35bfbc90369bfe530ee0e07abd46a6b6ecaae5e
Madush99 <madusankagajadeera99@gmail.com>
2021-05-28 00:34:23 +0530

    css

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
-import { useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
 import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
@@ -16,20 +16,20 @@ export const ProductCarousel = () => {
         dispatch(listTopProducts())
     }, [dispatch])
     return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-    <Carousel pause='hover' className='bg-dark'>
-        {products.map(product => (
-            <Carousel.Item key={product._id}>
-                <Link to={`/product/${product._id}`}>
-                    <Image src={product.image} alt={product.name} fluid />
-                    <Carousel.Caption className='carousel-caption'>
-                        <h2>
-                            {product.name} (${product.price})
+        <Carousel pause='hover' className='bg-dark'>
+            {products.map(product => (
+                <Carousel.Item key={product._id}>
+                    <Link to={`/product/${product._id}`}>
+                        <Image src={product.image} alt={product.name} fluid />
+                        <Carousel.Caption className='carousel-caption'>
+                            <h2>
+                                {product.name} (RS.{product.price})
                         </h2>
-                    </Carousel.Caption>
-                </Link>
-            </Carousel.Item>
-        ))}
-    </Carousel>)
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
+            ))}
+        </Carousel>)
 }
 
 export default ProductCarousel
